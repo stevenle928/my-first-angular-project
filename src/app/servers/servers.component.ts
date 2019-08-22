1524bd1088b276066ec51271d4fceb5681001e80
Steven <stevenle928@bitbucket.org>
2019-08-22 13:59:38 -0400

    learned about property binding and string interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,16 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  // templateUrl: './servers.component.html',
+  templateUrl: './servers.component.html',
   //Using template allows inline html in ts file, using backtick quotes allows you to wrap the code in multiple lines.
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  //constructor is simply a method executed at time of creation of this component by angular
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+   }
 
   ngOnInit() {
   }
